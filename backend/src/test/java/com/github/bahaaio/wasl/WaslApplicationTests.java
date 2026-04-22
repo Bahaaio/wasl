2dc99bae42284455cf0b9e73bb1d9c42273f986f
Bahaa Mohamed <bahaa-dev@proton.me>
2026-04-22 15:38:33 +0200

    chore: init

---
+++ backend/src/test/java/com/github/bahaaio/wasl/WaslApplicationTests.java
@@ -0,0 +1,12 @@
+package com.github.bahaaio.wasl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class WaslApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
