604bca363e1a9e93cca46eb4c8505bbfdddcf1b1
Bahaa Mohamed <bahaa-dev@proton.me>
2026-04-25 18:26:12 +0300

    feat: implement JwtService

---
+++ backend/src/main/java/com/github/bahaaio/wasl/auth/security/JwtService.java
@@ -0,0 +1,51 @@
+package com.github.bahaaio.wasl.auth.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class JwtService {
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+            .subject(username)
+            .issuedAt(new Date())
+            .expiration(new Date(System.currentTimeMillis() + expiration))
+            .signWith(getKey())
+            .compact();
+    }
+
+    public String extractUsername(String token) {
+        try {
+            return Jwts.parser()
+                .verifyWith((SecretKey) getKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+        } catch (JwtException ex) {
+            log.debug("failed to parse jwt: {}", ex.getMessage());
+            throw ex;
+        }
+    }
+
+    private Key getKey() {
+        return Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+}
