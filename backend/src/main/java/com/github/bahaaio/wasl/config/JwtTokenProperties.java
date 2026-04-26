60d15a4e88fdd3248214399a524945a38b26f993
Bahaa Mohamed <bahaa-dev@proton.me>
2026-04-27 00:50:12 +0300

    feat: add validation to JWT and refresh token properties

--- backend/src/main/java/com/github/bahaaio/wasl/config/JwtTokenProperties.java
+++ backend/src/main/java/com/github/bahaaio/wasl/config/JwtTokenProperties.java
@@ -1,14 +1,21 @@
 package com.github.bahaaio.wasl.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Validated
 @ConfigurationProperties("security.jwt")
 public class JwtTokenProperties {
+    @NotBlank
     private String secret;
+
+    @Positive
     private long expirationMs;
 }
--- backend/src/main/java/com/github/bahaaio/wasl/config/RefreshTokenProperties.java
+++ backend/src/main/java/com/github/bahaaio/wasl/config/RefreshTokenProperties.java
@@ -1,14 +1,20 @@
 package com.github.bahaaio.wasl.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Validated
 @ConfigurationProperties("security.refresh")
 public class RefreshTokenProperties {
+    @Positive
     private int size = 64;
+
+    @Positive
     private int expirationDays = 7;
 }
