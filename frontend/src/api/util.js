cc64a7b228a90d956658a75db73486d9e8325e94
Bahaa Mohamed <bahaa-dev@proton.me>
2026-05-15 20:30:19 +0300

    refactor(frontend): convert uploadFile() into async

--- frontend/src/api/util.js
+++ frontend/src/api/util.js
@@ -7,17 +7,17 @@ import api from "./client";
  * @param {File} file
  * @returns {Promise<UploadResponse>}
  */
-export const uploadFile = (url, file) => {
+export const uploadFile = async (url, file) => {
   const formData = new FormData();
   formData.append("file", file);
 
   // Ensure we don't send the instance default `application/json` header.
   // Setting Content-Type to `undefined` lets the browser set the correct
   // multipart/form-data boundary. Also increase timeout for larger files.
-  return api
-    .put(url, formData, {
-      headers: { "Content-Type": undefined },
-      timeout: 30000,
-    })
-    .then(res => res.data);
+  const res = await api.put(url, formData, {
+    headers: { "Content-Type": undefined },
+    timeout: 30000,
+  });
+
+  return res.data;
 };
