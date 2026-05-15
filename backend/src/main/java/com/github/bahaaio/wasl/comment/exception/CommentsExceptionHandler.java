b47eb142e62ed1ff76107a295c841acb4ddb6189
Bahaa Mohamed <bahaa-dev@proton.me>
2026-05-15 15:57:32 +0300

    fix(comments): return 404 for `CommentNotFound` exception

--- backend/src/main/java/com/github/bahaaio/wasl/comment/exception/CommentsExceptionHandler.java
+++ backend/src/main/java/com/github/bahaaio/wasl/comment/exception/CommentsExceptionHandler.java
@@ -2,6 +2,7 @@ package com.github.bahaaio.wasl.comment.exception;
 
 import com.github.bahaaio.wasl.exception.ApiError;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,8 +12,8 @@ public class CommentsExceptionHandler {
     // TODO: handle exceptions
     @ExceptionHandler(CommentNotFound.class)
     public ResponseEntity<ApiError<Void>> handleCommentNotFound(CommentNotFound ex) {
-        return ResponseEntity.ok(ApiError.of(
-            "COMMENT_NOT_FOUND", "Comment with id: " + ex.getId() + " not found"
-        ));
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+            ApiError.of("COMMENT_NOT_FOUND", "Comment with id: " + ex.getId() + " not found")
+        );
     }
 }
