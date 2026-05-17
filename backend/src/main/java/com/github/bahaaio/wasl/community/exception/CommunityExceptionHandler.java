64305d5557b2a2882d7ba5125196b8d5af88e537
Bahaa Mohamed <bahaa-dev@proton.me>
2026-05-17 18:28:18 +0300

    refactor(exception): add missing annotation

--- backend/src/main/java/com/github/bahaaio/wasl/community/exception/CommunityExceptionHandler.java
+++ backend/src/main/java/com/github/bahaaio/wasl/community/exception/CommunityExceptionHandler.java
@@ -5,7 +5,9 @@ import com.github.bahaaio.wasl.exception.ApiError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@RestControllerAdvice
 public class CommunityExceptionHandler {
     @ExceptionHandler(CommunityNotFoundException.class)
     public ResponseEntity<ApiError<Void>> handleCommunityNotFoundException(CommunityNotFoundException ex) {
