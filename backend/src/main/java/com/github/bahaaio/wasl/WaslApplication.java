9a9b9c81b28ada0d7db77e6b8f01ea1f09787434
Bahaa Mohamed <bahaa-dev@proton.me>
2026-05-18 13:54:49 +0300

    feat(backend): enable scheduling

--- backend/src/main/java/com/github/bahaaio/wasl/WaslApplication.java
+++ backend/src/main/java/com/github/bahaaio/wasl/WaslApplication.java
@@ -3,9 +3,11 @@ package com.github.bahaaio.wasl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@EnableScheduling
 @ConfigurationPropertiesScan
+@SpringBootApplication
 public class WaslApplication {
     public static void main(String[] args) {
         SpringApplication.run(WaslApplication.class, args);
